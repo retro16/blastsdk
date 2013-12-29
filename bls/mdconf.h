90bd73b8cbbe0cba65c15768dd90200412890fe1
Jean-Matthieu COULON <git@coulon-jm.fr>
2013-12-29 15:56:28 +0100

    added mdconf.h, added prev to mdconfnode, made node->key variable length.

---
+++ bls/mdconf.h
@@ -0,0 +1,22 @@
+#ifndef MDCONF_H
+#define MDCONF_H
+
+typedef struct mdconfnode {
+  char *key;
+  char *value;
+  struct mdconfnode *prev;
+  struct mdconfnode *next;
+  struct mdconfnode *child;
+  struct mdconfnode *parent;
+} mdconfnode;
+
+mdconfnode * mdconfappendsibling(mdconfnode *node);
+mdconfnode * mdconfappendchild(mdconfnode *node);
+mdconfnode * mdconfcopy(const mdconfnode *node);
+void mdconffree(mdconfnode *root);
+mdconfnode * mdconfparsefile(const char *filename);
+const mdconfnode * mdconfsearch(const mdconfnode *node, const char *path);
+const char *mdconfget(const mdconfnode *root, const char *path);
+void mdconfprint(const mdconfnode *root, int depth);
+
+#endif//MDCONF_H
